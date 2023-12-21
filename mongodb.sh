23d5f3fe2b715290e91b895d7cbd70a8308bb583
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-21 20:28:56 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
     echo -e "\e[32mCopying the mongo repo file to the path\e[0m"
     cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+
     echo -e "\e[32mInstalling the mongo server\e[0m"
     yum install mongodb-org -y
     ## Modify the configuration file /etc/mongod.conf
