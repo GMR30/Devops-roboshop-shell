970253a2e1de20a01fcd7ff9ac6ad2406978de2a
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-21 19:55:08 +0530

    add a valid comment

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,16 @@
     #Step1
     echo -e "\e[32mInstalling ngnix server\e[0m"
-    yum install nginx -y >/tmp/roboshop.log
+    yum install nginx -y &>>/tmp/roboshop.log
     echo -e "\e[32mremoving existing content\e[0m"
-    rm -rf /usr/share/nginx/html/*  >/tmp/roboshop.log
+    rm -rf /usr/share/nginx/html/*  &>>/tmp/roboshop.log
     echo -e "\e[32mDown loadning new version\e[0m"
-    curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/roboshop.log 2>/tmp/error.log
+    curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
     echo -e "\e[32mExtracting the downloaded version\e[0m"
-    cd /usr/share/nginx/html  >/tmp/roboshop.log
-    unzip /tmp/frontend.zip  >/tmp/roboshop.log
+    cd /usr/share/nginx/html  &>>/tmp/roboshop.log
+    unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
     echo -e "\e[32mCopying the configuration file\e[0m"
     #Copy a configuration file
-    systemctl enable nginx   >/tmp/roboshop.log
-    systemctl restart nginx  >/tmp/roboshop.log
+    systemctl enable nginx   &>>/tmp/roboshop.log
+    systemctl restart nginx  &>>/tmp/roboshop.log
 
 
