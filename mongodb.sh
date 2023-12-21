4b8bcaf7c1e23ea7841489e078dec351623bb704
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-21 21:01:51 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
     echo -e "\e[32mcpying the repofile to yum repos\e[0m"
-    cp /home/centos/Devops-githubrepos/Devops-roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
+    cp /home/centos/Devops-roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
     yum install mongodb-org -y
     systemctl enable mongod
     systemctl restart mongod
