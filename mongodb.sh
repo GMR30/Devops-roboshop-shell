0a75964d6539a0acafa2afdf6d68192faf2b9c48
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-22 10:43:30 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,6 @@
 
     echo -e "\e[32mcpying the repofile to yum repos\e[0m"
     cp mongodb.repo  /etc/yum.repos.d/mongodb.repo
-    cat /etc/yum.repos.d/mongodb-org-4.repo
-    dnf install mongodb-org -y
+    yum install mongodb-org -y
     systemctl enable mongod
     systemctl restart mongod
