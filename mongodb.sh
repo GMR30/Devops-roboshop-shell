2d7b12899c250baf51c5a0482da172e2c57548da
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-22 10:16:52 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,4 @@
+    rpm --import https://www.mongodb.org/static/pgp/server-4.2.asc
     echo -e "\e[32mcpying the repofile to yum repos\e[0m"
     cp mongodb.repo  /etc/yum.repos.d/mongodb.repo
     cat /etc/yum.repos.d/mongodb.repo
