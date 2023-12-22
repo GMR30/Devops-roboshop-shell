3a140392464fa8ea2e197e05060a9ecf7275c4f3
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-22 10:33:31 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,6 @@
 
-    rpm --import https://www.mongodb.org/static/pgp/server-4.2.asc
     echo -e "\e[32mcpying the repofile to yum repos\e[0m"
-    cp mongodb.repo  /etc/yum.repos.d/mongodb.repo
+    cp mongodb.repo  /etc/yum.repos.d/mongodb-org-4.repo
     cat /etc/yum.repos.d/mongodb.repo
     yum -y install mongodb-org
     systemctl enable mongod
