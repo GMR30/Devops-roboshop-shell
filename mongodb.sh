6c84d31d8b245b6e7c76754273ad470056e08230
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-26 18:59:56 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -2,7 +2,7 @@ echo -e "\e[32mCopying the repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[32mInstalling Mongodb server\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
-echo -e "\e[32mEdit the Configuration file\e[0m"
+echo -e "\e[32mEdit the listen address\e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 echo -e "\e[32mRestart the service\e[0m"
