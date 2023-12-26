2ddc4498aaf58b5c9da2633223ca23aa53d78145
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-26 18:57:27 +0530

    add a valid comment

--- mongodb.sh
+++ mongodb.sh
@@ -3,6 +3,7 @@ cp mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[32mInstalling Mongodb server\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 echo -e "\e[32mEdit the Configuration file\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 echo -e "\e[32mRestart the service\e[0m"
 systemctl restart mongod  &>>/tmp/roboshop.log
