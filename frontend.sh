06b0c27141a95b6e27939a7241564bb0f847b644
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-27 19:57:44 +0530

    Installing user revice

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,6 @@ echo -e "\e[32mDown loadning new version\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
 echo -e "\e[32mExtracting the downloaded version\e[0m"
 cd /usr/share/nginx/html
-pwd
 unzip /tmp/frontend.zip
 unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
 echo -e "\e[32mCopying the configuration file\e[0m"
