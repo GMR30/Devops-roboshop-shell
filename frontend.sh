e00722cd09f05e2a3e842f568c5ed3b73b0a8d5e
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-27 19:47:18 +0530

    Installing user revice

--- frontend.sh
+++ frontend.sh
@@ -2,11 +2,13 @@
 echo -e "\e[32mInstalling ngnix server\e[0m"
 yum install nginx -y &>>/tmp/roboshop.log
 echo -e "\e[32mremoving existing content\e[0m"
-rm -rf /usr/share/nginx/html/*  &>>/tmp/roboshop.log
+rm -rf /usr/share/nginx/html/*   &>>/tmp/roboshop.log
 echo -e "\e[32mDown loadning new version\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
 echo -e "\e[32mExtracting the downloaded version\e[0m"
-cd /usr/share/nginx/html  &>>/tmp/roboshop.log
+cd /usr/share/nginx/html
+pwd
+unzip /tmp/frontend.zip
 unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
 echo -e "\e[32mCopying the configuration file\e[0m"
 cp /root/Devops-roboshop-shell/roboshop.conf  /etc/nginx/default.d/roboshop.conf
