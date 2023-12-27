8feac060d8a94ff3e13f6cd1c7307c9166190512
Raja Bhushnam <rajabhushanam117@gmail.com>
2023-12-27 18:16:31 +0530

    add a valid comment

---
+++ redis.sh
@@ -0,0 +1,12 @@
+echo -e "\e[32mInstall the repository\e[0m"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[32mEnable the redis module\e[0m"
+dnf module enable redis:remi-6.2 -y
+echo -e "\e[32mInstalling the redis\e[0m"
+dnf install redis -y
+echo -e "\e[32mEdit the redis listen address\e[0m"
+sed -i  's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+echo -e "\e[32menable the redis\e[0m"
+systemctl enable redis
+echo -e "\e[32mRestart the redis service\e[0m"
+systemctl restart redis
