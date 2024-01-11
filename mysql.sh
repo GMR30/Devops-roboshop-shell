d2e962bc723fa61a7a488711d4c89504e1d54771
Raja Bhushnam <rajabhushanam117@gmail.com>
2024-01-11 09:48:44 +0530

    Installing  the  Mysql server and  service

---
+++ mysql.sh
@@ -0,0 +1,12 @@
+echo -e "\e[32mDisable the mysql module\e[0m"
+dnf module disable mysql -y
+echo -e "\e[32mInstall the mysql community server\e[0m"
+dnf install mysql-community-server -y
+echo -e "\e[32mmysql service enabled \e[0m"
+systemctl enable mysqld
+echo -e "\e[32mstartthe mysql server\e[0m"
+systemctl restart mysqld
+echo -e "\e[32mSet_Root_Passwd the mysql community server\e[0m"
+mysql_secure_installation --set-root-pass RoboShop@1
+#echo -e "\e[32mLogin to the mysql community server\e[0m"
+#mysql -uroot -pRoboShop@1
