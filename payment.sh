bd6807887008e35c3d9af2fed68d949032688fc7
Raja Bhushnam <rajabhushanam117@gmail.com>
2024-01-11 11:25:28 +0530

    Creating the payment service

--- payment.sh
+++ payment.sh
@@ -3,6 +3,7 @@ dnf install python36 gcc python3-devel -y
 echo -e "\e[32mAdding roboshop user\e[0m"
 useradd roboshop
 echo -e "\e[32mcreating app folder\e[0m"
+rm -rf /app
 mkdir /app
 echo -e "\e[32mDownloadning payment zip file\e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
@@ -10,7 +11,7 @@ echo -e "\e[32mChaning the dir\e[0m"
 cd /app
 echo -e "\e[32mUnzip the file\e[0m"
 unzip /tmp/payment.zip
-#cd /app
+cd /app
 echo -e "\e[32mInstalling the pip3\e[0m"
 pip3.6 install -r requirements.txt
 echo -e "\e[32mCopying the service file\e[0m"
