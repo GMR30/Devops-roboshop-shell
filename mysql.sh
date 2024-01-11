54afc7b71464abe3831dc41a6f7d1f12cfcafe7a
Raja Bhushnam <rajabhushanam117@gmail.com>
2024-01-11 10:32:08 +0530

    Installing  the  shipping service

--- mysql.sh
+++ mysql.sh
@@ -10,5 +10,5 @@ echo -e "\e[32mstartthe mysql server\e[0m"
 systemctl restart mysqld
 echo -e "\e[32mSet_Root_Passwd the mysql community server\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
-#echo -e "\e[32mLogin to the mysql community server\e[0m"
-#mysql -uroot -pRoboShop@1
+echo -e "\e[32mLogin to the mysql community server\e[0m"
+mysql -uroot -pRoboShop@1
